0ebc17a5ad8a453f3b74afd78cc4c3763f506d60
Adn086 <alu0100417012@ull.edu.es>
2023-11-30 22:13:53 +0000

    Actualuzacion metodos Observadores

--- src/main/java/P7/PantallaMetereologica.java
+++ src/main/java/P7/PantallaMetereologica.java
@@ -1,9 +1,14 @@
 package P7;
 
+import org.json.JSONObject;
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
-public class PantallaMeteorologica implements Observador {
+public class PantallaMetereologica implements Observador {
     private JFrame frame;
     private JLabel labelCiudad;
     private JLabel labelHora;
@@ -68,5 +73,74 @@ public class PantallaMeteorologica implements Observador {
 
         frame.setSize(470, 362);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actualizar(String condicionesJSON) {
+        try {
+            JSONObject jsonCompleto = new JSONObject(condicionesJSON);
+
+            // Accediendo a los objetos 'current' y 'location'
+            JSONObject current = jsonCompleto.getJSONObject("current");
+            JSONObject location = jsonCompleto.getJSONObject("location");
+
+            // Extrayendo datos de 'current'
+            String temperatura = "Temp: " + current.optInt("temperature", 0) + "°C";
+            String humedad = "Humedad: " + current.optInt("humidity", 0) + "%";
+            String iconoURL = current.getJSONArray("weather_icons").optString(0, "");
+
+            // Extrayendo la hora local de 'location'
+            String horaLocal = location.optString("localtime", "No disponible");
+            String horaObservacion = extraerHora(horaLocal);
+
+            SwingUtilities.invokeLater(() -> {
+                labelTemperatura.setText(temperatura);
+                labelHumedad.setText(humedad);
+                labelHora.setText(horaObservacion);
+
+                if (!iconoURL.isEmpty()) {
+                    cargarIcono(iconoURL);
+                } else {
+                    labelIcono.setText("Icono no disponible");
+                }
+            });
+            mostrarAlertaSiNecesario(current);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al procesar la respuesta JSON: " + condicionesJSON);
+        }
+    }
+
+    private String extraerHora(String horaCompleta) {
+        // Asume un formato 'YYYY-MM-DD HH:MM' y extrae solo la parte de la hora
+        int spaceIndex = horaCompleta.indexOf(' ');
+        return spaceIndex != -1 ? horaCompleta.substring(spaceIndex + 1) : "Formato inválido";
+    }
+
+
+
+
+    private void cargarIcono(String iconoURL) {
+        try {
+            URL url = new URL(iconoURL);
+            ImageIcon icono = new ImageIcon(url);
+            Image img = icono.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+            labelIcono.setIcon(new ImageIcon(img));
+        } catch (Exception e) {
+            e.printStackTrace();
+            labelIcono.setText("Error al cargar icono");
+        }
+    }
+
+    private void mostrarAlertaSiNecesario(JSONObject current) {
+        int temperaturaActual = current.optInt("temperature", 0);
+
+        if (temperaturaActual > 25) {
+            labelAlerta.setText("¡Alerta de Calor! Considere tomar precauciones.");
+        }else if (temperaturaActual < 5) {
+            labelAlerta.setText("¡Alerta de Frio! Considere tomar precauciones.");
+        } else {
+            labelAlerta.setText("");
+        }
     }
 }
