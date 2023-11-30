2a76aa0a4846328a892442b216891bd54287e245
alu0101330600 <65622828+jordidogo@users.noreply.github.com>
2023-11-30 19:52:43 +0000

    Implementada Api con Key no funcional

---
+++ src/main/java/P7/WeatherStackAPI.java
@@ -0,0 +1,31 @@
+package P7;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class WeatherStackAPI {
+    String apiKey = "5cfbdb768ce502c32fb609af82754840";
+    String apiUrl = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=";
+
+    public String getWeatherConditionsFrom(String zone) {
+        try {
+            String ciudadCodificada = URLEncoder.encode(zone, StandardCharsets.UTF_8.toString());
+            HttpResponse<JsonNode> response = Unirest.get(apiUrl + ciudadCodificada)
+                    .header("Accept", "application/json")
+                    .asJson();
+            // System.out.println(response.getBody().toString()); //comprobacion info por pantalla
+            return getWeatherConditionFromResponse(response.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Not provided";
+        }
+    }
+
+    private static String getWeatherConditionFromResponse(JsonNode jsonResponse) {
+        return jsonResponse.getObject().toString();
+    }
+}
---
+++ src/main/java/P7/WeatherstackExample.java
@@ -0,0 +1,10 @@
+package P7;
+
+import java.io.IOException;
+
+public class WeatherstackExample {
+
+    public static void main(String[] args) throws IOException {
+        new SeleccionCiudad();
+    }
+}
